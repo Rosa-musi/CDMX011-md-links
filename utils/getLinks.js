f8271776d694bd5e5a1a1e3536bca2dc0750ccad
Rosa-musi <nadamejor27@hotmail.com>
2021-10-09 12:23:24 -0500

    comentarios

--- utils/getLinks.js
+++ utils/getLinks.js
@@ -15,23 +15,20 @@ const cheerio = require('cheerio')
 //crear un array de links.
 
 const getLinks = (path) => {
-    let absolutePath = (isAbsolute(path) ? path : resolve(path))
-    let paths = readMd(absolutePath)
-    let links = []
-    //console.log(absolutePath)
+    let absolutePath = (isAbsolute(path) ? path : resolve(path)) //haciendo la ruta absoluta
+    let paths = readMd(absolutePath)  //obteniendo las rutas de los archivos md con la anterior función
+    let links = []  //array para recolectar la información de los links obtenidos
     paths.forEach((element) => {
-        let file = readFileSync(element, 'utf-8')
-        let html = marked(file)
-        const $ = cheerio.load(html)
-        let baseLink = element
-        $('a').each((index, element) => {
-            let href = $(element).attr('href')
+        let file = readFileSync(element, 'utf-8') //lectura de archivos md
+        let html = marked(file)                   //conversión de archivos md a html con marked
+        const $ = cheerio.load(html)              //cargar archivo html a cheerio para el scraping
+        let baseLink = element                    //variable con el link dónde está el archivo para crear el objeto el link
+        $('a').each((index, element) => {         //iterar por cada <a> encontrado en mi html 
+            let href = $(element).attr('href')    //crando variables para los datos que necesito del <a>
             let text = $(element).text()
             let link = baseLink
-            links.push({href: href, text: text, file: link})
-            //console.log(links)
+            links.push({href: href, text: text, file: link})  //creando objeto para hacer push en mi array de links
         })
-        //console.log(links)
     })
     return links
 } 
