0a541de45f22e5b23541b92f952a140689243105
Rosa-musi <nadamejor27@hotmail.com>
2021-10-13 10:08:22 -0500

    terminado

---
+++ utils/StatsValidate.js
@@ -0,0 +1,40 @@
+const validate = require('./validate')
+//Crear una función que muestre:
+//total de links
+//Links únicos
+//links rotos
+
+
+
+const validateStats = (links) => {
+    let linksVal = links.map(link => {
+        let validationPromise = validate(link)
+        return validationPromise
+    })         
+    let response = Promise.all(linksVal).then(values =>{
+        href = values.map((element => {
+            return element.href
+        }))
+        const uniqueFilter = new Set(href)
+        let unique = [...uniqueFilter]
+
+        let broken = []
+        values.map(link => {
+            if (link.response >= 400 && link.response <= 500){
+                broken.push(link)
+            }
+        })
+
+        let response = {
+            Total: values.length, 
+            Unique: unique.length,
+            Broken: broken.length
+        }
+
+        return response
+    })
+    
+    return response
+}
+
+module.exports = validateStats
